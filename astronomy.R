2e2072b550d1edcc5ed6f6e8d527666fbc2c4b91
Stainley Lebron <stainley_lebron@claro.com.do>
2020-02-05 16:43:40 -0400

    Added information

--- astronomy.R
+++ astronomy.R
@@ -1,5 +1,20 @@
-# Title     : TODO
+# Title     : Stars Exercises
 # Objective : TODO
 # Created by: Stainley Lebron
 # Created on: 2/5/2020
 
+library(tidyverse)
+library(dslabs)
+data(stars)
+options(digits = 3)   # report 3 significant digits
+
+names(stars)
+class("magnitude")
+mean(stars$magnitude)
+sd(stars$magnitude)
+
+stars %>% ggplot(aes(magnitude)) + geom_density()
+
+stars %>% ggplot(aes(temp)) + geom_density()
+
+stars %>% ggplot(aes(x=temp, y=magnitude)) + geom_point()
