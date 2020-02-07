40dad79f55beddcdb1a9134031b9a2ce1039c7ef
Stainley Lebron <stainley_lebron@claro.com.do>
2020-02-07 13:42:28 -0400

    Added vline to show a point in the year

--- climate.R
+++ climate.R
@@ -16,6 +16,7 @@ data(greenhouse_gases)
 data(historic_co2)
 
 names(greenhouse_gases)
+names(temp_carbon)
 
 greenhouse_gases %>% ggplot(aes(x = year, y = concentration)) +
   geom_line() +
@@ -23,3 +24,9 @@ greenhouse_gases %>% ggplot(aes(x = year, y = concentration)) +
   geom_vline(xintercept = 1850)
   ylab("Concentration (ch4/n2o ppb, co2 ppm") +
   ggtitle("Atmospheric greenhouse gas concentration by year,0-2000")
+
+
+
+temp_carbon %>% ggplot(aes(x=year,y=carbon_emissions, color=year)) + geom_line(na.rm = TRUE) + geom_vline(xintercept = 1960, show.legend = year)
+
+
