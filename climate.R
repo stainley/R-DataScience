419104923c6706dc39c16ff96d4021c222fdb38c
Stainley Lebron <stainley_lebron@claro.com.do>
2020-02-11 10:01:57 -0400

    probability

--- climate.R
+++ climate.R
@@ -20,13 +20,23 @@ names(temp_carbon)
 
 greenhouse_gases %>% ggplot(aes(x = year, y = concentration)) +
   geom_line() +
-  facet_grid(gas ~ . , scales = "free") +
+  facet_grid(gas ~ ., scales = "free") +
   geom_vline(xintercept = 1850)
-  ylab("Concentration (ch4/n2o ppb, co2 ppm") +
+ylab("Concentration (ch4/n2o ppb, co2 ppm") +
   ggtitle("Atmospheric greenhouse gas concentration by year,0-2000")
 
 
+temp_carbon %>% ggplot(aes(x = year, y = carbon_emissions, color = year)) +
+  geom_line(na.rm = TRUE) +
+  geom_vline(xintercept = 1960, show.legend = year)
 
-temp_carbon %>% ggplot(aes(x=year,y=carbon_emissions, color=year)) + geom_line(na.rm = TRUE) + geom_vline(xintercept = 1960, show.legend = year)
 
+names(historic_co2)
 
+co2_time <- historic_co2 %>% ggplot(aes(x = year, y = co2, color = source)) +
+  geom_line() +
+  ggtitle("Atmospheric CO2 concentration, -800,000 BC to today") +
+  geom_hline(yintercept = 200) +
+  ylab("co2 (ppmv)")
+
+co2_time + xlim(-3000,  2018)
--- probability.R
+++ probability.R
@@ -3,3 +3,69 @@
 # Created by: Stainley Lebron
 # Created on: 2/10/2020
 
+library(gtools)
+library(tidyverse)
+options(digits = 3)
+
+
+# 1a
+medals <- permutations(8, 3)
+nrow(medals)
+
+# 1b
+medals <- permutations(3, 3)
+nrow(medals)
+
+# 1c
+jamaica <- c('usain', 'yohan', 'warren')
+other_countries <- paste('c', as.character(1:5))
+countries <- c(jamaica, other_countries)
+
+places <- permutations(8, 3, v=countries)
+table(places)
+class(places)
+
+first_place <- places[,1]
+second_place <- places[,2]
+third_place <- places[,3]
+
+mean(first_place %in% jamaica & second_place %in% jamaica & third_place %in% jamaica)
+
+
+# 1d
+runners <- c("Jamaica", "Jamaica", "Jamaica", "USA", "Ecuador", "Netherlands", "France", "South Africa")
+set.seed(1)
+
+simulation <- replicate(10000, {
+  winners <- sample(runners, 3)
+  sum(winners == 'Jamaica') == 3
+})
+
+mean(simulation)
+
+# 2a
+# 6 options, 2 options,
+#s 1 entree, 2 sides, and 1 drink
+#1 entree from a list of 6 options, a choice of 2 different sides from a list of 6 options,
+# and a choice of 1 drink from a list of 2 options.
+6 * nrow(combinations(6, 2)) * 2
+6 * nrow(combinations(6, 2)) * 3
+d <- 6 * nrow(combinations(6, 3)) * 3
+
+# 2d
+dishes <- function (entree) {
+  entree * nrow(combinations(6, 2)) * 3
+}
+
+data <- sapply(1:12, dishes)
+df <- data.frame(entry=1:12, combo=data)
+df %>% filter(combo > 365) %>% min(.$entry)
+
+# 2e
+side <- function (side) {
+  6 * nrow(combinations(side, 2)) * 3
+}
+
+dt <- sapply(2:12, side)
+df <- data.frame(entry=2:12, combo=dt)
+df %>% filter(combo > 365) %>% min(.$entry)
